25f8cf8a6203a166a114bf0b2c0bf2d06feb6550
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2020-12-01 18:07:05 +0100

    crud methods for type class

--- src/main/java/main/kts/service/TypeService.java
+++ src/main/java/main/kts/service/TypeService.java
@@ -2,41 +2,60 @@ package main.kts.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import main.kts.model.Type;
+import main.kts.repository.TypeRepository;
 
 @Service
 public class TypeService implements ServiceInterface<Type>{
+	
+	@Autowired
+	private TypeRepository typeRepository;
 
 	@Override
 	public List<Type> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return typeRepository.findAll();
 	}
 
 	@Override
 	public Type findOne(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return typeRepository.findById(id).orElse(null);
 	}
 
 	@Override
 	public Type create(Type entity) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Type t = new Type();
+		// name, descripton, category
+		t.setName(entity.getName());
+		t.setDescription(entity.getDescription());
+		t.setCategory(entity.getCategory());
+		return t;
 	}
 
 	@Override
 	public Type update(Type entity, Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Type existingT = typeRepository.findById(id).orElse(null);
+		if(existingT == null) {
+			throw new Exception("Type eith given id doesn't exist");
+		}
+		existingT.setName(entity.getName());
+		existingT.setDescription(entity.getDescription());
+		existingT.setCategory(entity.getCategory());
+		
+		return typeRepository.save(existingT);
 	}
 
 	@Override
 	public void delete(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		
+		Type existingT = typeRepository.findById(id).orElse(null);
+		if(existingT == null) {
+			throw new Exception("Type eith given id doesn't exist");
+		}
+		typeRepository.delete(existingT);
 	}
+
+	
 	
 }
