781056523d0aefddadf788987e002c3acd4aa84d
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2020-12-18 23:01:03 +0100

    junit tests for category service

---
+++ server/src/test/java/main/kts/constants/CategoryConstants.java
@@ -0,0 +1,30 @@
+package main.kts.constants;
+
+public class CategoryConstants {
+	
+	 public static final String DB_CATEGORY_NAME = "institution";
+	 public static final String DB_CATEGORY_DESCIPTION = "institutions in serbia";
+	 public static final Long DB_CATEGORY_ID = 1L;
+	 
+	 public static final String DB_CATEGORY_NAME2 = "manifestation";
+	 public static final String DB_CATEGORY_DESCIPTION2 = "manifestations in serbia";
+	 public static final Long DB_CATEGORY_ID2 = 2L;
+	
+	 public static final String NEW_CATEGORY_NAME = "festival";
+	 public static final String NEW_CATEGORY_DESCRIPTION = "music festival";
+	 public static final Long NEW_CATEGORY_ID = 3L;
+	 
+	 public static final String UPDATE_CATEGORY_NAME = "government institution";
+	
+	 public static final long FIND_ALL_NUMBER_OF_ITEMS = 2;
+	
+	 public static final Integer PAGEABLE_PAGE = 0;
+     public static final Integer PAGEABLE_SIZE = 2;
+     public static final Integer PAGEABLE_TOTAL_ELEMENTS = 1;
+	
+	 public static final Long CATEGORY_ID = 1L;
+	 
+	 public static final Long FALSE_CATEGORY_ID = 4L;
+	
+
+}
