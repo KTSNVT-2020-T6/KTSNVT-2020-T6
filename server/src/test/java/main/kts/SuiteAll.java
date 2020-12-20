6efa93c8393fad22663319c3f54a6e63516cc00e
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2020-12-20 15:19:46 +0100

    Added tests with wrong params and solved "start_value" exception.

--- server/src/test/java/main/kts/SuiteAll.java
+++ server/src/test/java/main/kts/SuiteAll.java
@@ -7,7 +7,9 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.TestPropertySource;
 
 import main.kts.controller.AdminControllerIntegrationTest;
+import main.kts.controller.SuiteControllers;
 import main.kts.repository.AdminRepositoryIntegrationTest;
+import main.kts.repository.SuiteRepositories;
 import main.kts.service.AdminServiceIntegrationTest;
 import main.kts.service.AdminServiceUnitTest;
 import main.kts.service.CategoryServiceUnitTest;
@@ -17,6 +19,7 @@ import main.kts.service.ImageServiceUnitTest;
 import main.kts.service.PostServiceUnitTest;
 import main.kts.service.RateServiceUnitTest;
 import main.kts.service.RegisteredServiceUserUnitTest;
+import main.kts.service.SuiteServices;
 
 @RunWith(Suite.class)
 @SuiteClasses({SuiteRepositories.class,SuiteServices.class,SuiteControllers.class})
--- server/src/test/java/main/kts/repository/AdminRepositoryIntegrationTest.java
+++ server/src/test/java/main/kts/repository/AdminRepositoryIntegrationTest.java
@@ -1,9 +1,14 @@
 package main.kts.repository;
 
 import static main.kts.constants.AdminConstants.DB_ADMIN_EMAIL;
+import static main.kts.constants.AdminConstants.DB_ADMIN_EMAIL_DoesntExist;
 import static main.kts.constants.AdminConstants.DB_ADMIN_ID;
+import static main.kts.constants.AdminConstants.DB_ADMIN_ID_DoesntActive;
 import static main.kts.constants.AdminConstants.DB_ADMIN_SIZE;
+import static main.kts.constants.AdminConstants.NEW_ADMIN_ID1;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +16,7 @@ import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -35,17 +35,34 @@ public class AdminRepositoryIntegrationTest {
 		Admin foundAdmin = repository.findByEmail(DB_ADMIN_EMAIL);
 		assertEquals(DB_ADMIN_EMAIL, foundAdmin.getEmail());
 	}
+	@Test
+	public void testFindByEmail_EmailDoesntExists() {
+		Admin foundAdmin = repository.findByEmail(DB_ADMIN_EMAIL_DoesntExist);
+		assertNull(foundAdmin);
+	}
 	
 	@Test
 	public void testFindAllAdmin() {
 		List<Admin> foundAdmins = repository.findAllAdmin();
 	    assertEquals(DB_ADMIN_SIZE, foundAdmins.size());
 	}
-	
+		
 	@Test
 	public void testFindByIdAndActive() {
 		Optional<Admin> foundAdmin = repository.findByIdAndActive(DB_ADMIN_ID, true);
 		assertEquals(DB_ADMIN_ID, foundAdmin.get().getId());
+	}
+	
+	@Test
+	public void testFindByIdAndActive_IdDoesntExist() {
+		Optional<Admin> foundAdmin = repository.findByIdAndActive(NEW_ADMIN_ID1, true);
+		assertFalse(foundAdmin.isPresent());
+	}
+	
+	@Test
+	public void testFindByIdAndActive_IdExistButNotActive() {
+		Optional<Admin> foundAdmin = repository.findByIdAndActive(DB_ADMIN_ID_DoesntActive, true);
+		assertFalse(foundAdmin.isPresent());
 	}
 	
 }
