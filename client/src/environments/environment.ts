8b4377c84a8c46811c16b48f8ca6782d1da6252c
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2021-01-24 21:55:51 +0100

    ssl certificate enabled BUT NOT ON MY CHROME

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://localhost:8443',
   login: 'auth/log-in',
   signUp: 'auth/sign-up',
   signUpAdmin: 'auth/sign-up-admin',
